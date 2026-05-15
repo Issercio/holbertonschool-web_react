b036090d74e9b5062a59ce59e5402d88f202118e
Issercio <didi_1374@outlook.fr>
2026-05-15 09:39:00 +0200

    add the project

--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -4,47 +4,47 @@ import App from './App';
 describe('App component', () => {
   test('Vérification texte h1 App-header', () => {
     render(<App />);
-    const headerh1 = screen.getByRole('heading', { level: 1, name: /School dashboard/i });
+    const headerh1 = screen.getByRole('heading', { level: 1, name: /School dashboard/ });
     expect(headerh1).toBeInTheDocument();
   });
 
   test('Vérification texte App-body', () => {
     render(<App />);
-    const bodyp = screen.getByText(/Login to access the full dashboard/i);
+    const bodyp = screen.getByText(/Login to access the full dashboard/);
     expect(bodyp).toBeInTheDocument();
   });
 
   test('Vérification texte App-footer', () => {
     render(<App />);
-    const footerp = screen.getByText(/Copyright \d{4} - holberton School/i);
+    const footerp = screen.getByText(/Copyright \d{4} - holberton School/);
     expect(footerp).toBeInTheDocument();
   });
 
   test('Vérification alt image App-header', () => {
     render(<App />);
-    const headerImgAlt = screen.getByAltText(/holberton logo/i);
+    const headerImgAlt = screen.getByAltText(/holberton logo/);
     expect(headerImgAlt).toBeInTheDocument();
   });
 
   test('Vérification des inputs associés aux labels', () => {
     render(<App />);
-    const emailInput = screen.getByLabelText(/email/i);
-    const passwordInput = screen.getByLabelText(/password/i);
+    const emailInput = screen.getByLabelText(/email/);
+    const passwordInput = screen.getByLabelText(/password/);
     expect(emailInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
   });
 
   test('Vérification du texte des labels', () => {
     render(<App />);
-    const emailLabel = screen.getByLabelText(/email/i);
-    const passwordLabel = screen.getByLabelText(/password/i);
+    const emailLabel = screen.getByLabelText(/email/);
+    const passwordLabel = screen.getByLabelText(/password/);
     expect(emailLabel).toBeInTheDocument();
     expect(passwordLabel).toBeInTheDocument();
   });
 
   test('Vérification du bouton', () => {
     render(<App />);
-    const formButton = screen.getByRole('button', { name: /OK/i });
+    const formButton = screen.getByRole('button', { name: /OK/ });
     expect(formButton).toBeInTheDocument();
   });
 });
