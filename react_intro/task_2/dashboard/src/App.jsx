73ad945792992505ea9dcc491d64f738287bf819
Issercio <didi_1374@outlook.fr>
2026-05-15 09:57:29 +0200

    add the project

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -9,29 +9,29 @@ function App() {
 
   return (
     <>
-      <div className="root-notifications">
-        <Notifications />
-      </div>
+      <Notifications />
 
-      <div className='App-header'>
-        <img src={HbSLogo} alt="holberton logo" />
-        <h1>School dashboard</h1>
-      </div>
+      <div className="App">
+        <div className="App-header">
+          <img src={HbSLogo} alt="holberton logo" />
+          <h1>School dashboard</h1>
+        </div>
 
-      <div className='App-body'>
-        <p>Login to access the full dashboard</p>
+        <div className="App-body">
+          <p>Login to access the full dashboard</p>
 
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" />
 
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" />
 
-        <button>Ok</button>
-      </div>
+          <button>Ok</button>
+        </div>
 
-      <div className='App-footer'>
-        <p>Copyright {currentYear} - {footerText}</p>
+        <div className="App-footer">
+          <p>Copyright {currentYear} - {footerText}</p>
+        </div>
       </div>
     </>
   );
