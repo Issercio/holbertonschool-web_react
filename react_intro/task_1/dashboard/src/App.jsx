1545f155efdce60bf931b2c0ff81dd4706dbc843
Issercio <didi_1374@outlook.fr>
2026-05-15 09:19:51 +0200

    add the project

--- react_intro/task_1/dashboard/src/App.jsx
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -10,15 +10,20 @@ function App() {
       <div className="root-notifications">
         <Notifications />
       </div>
+
       <div className="App-header">
         <img src={logo} className="App-logo" alt="holberton logo" />
         <h1>School dashboard</h1>
       </div>
+
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
+
       <div className="App-footer">
-        <p>Copyright {getCurrentYear()} {getFooterCopy(false)}</p>
+        <p>
+          Copyright {getCurrentYear()} - {getFooterCopy(false)}
+        </p>
       </div>
     </>
   );
--- react_intro/task_1/dashboard/src/utils.js
+++ react_intro/task_1/dashboard/src/utils.js
@@ -1,13 +1,9 @@
-const getCurrentYear = () => new Date().getFullYear();
+export function getCurrentYear() {
+  return new Date().getFullYear();
+}
 
-const getFooterCopy = (isIndex) =>
-  isIndex
+export function getFooterCopy(isIndex) {
+  return isIndex
     ? 'Holberton School'
     : 'Holberton School main dashboard';
-
-export { getCurrentYear, getFooterCopy };
-
-export default {
-  getCurrentYear,
-  getFooterCopy,
-};
+}
