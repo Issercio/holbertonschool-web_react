20bdcf6d511c8ad0a90f8c6b580b6b5d19287fd8
Issercio <didi_1374@outlook.fr>
2026-05-15 09:59:24 +0200

    add the project

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -8,32 +8,32 @@ function App() {
   const footerText = getFooterCopy(true);
 
   return (
-    <>
-      <Notifications />
-
-      <div className="App">
-        <div className="App-header">
-          <img src={HbSLogo} alt="holberton logo" />
-          <h1>School dashboard</h1>
-        </div>
-
-        <div className="App-body">
-          <p>Login to access the full dashboard</p>
-
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
-
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" />
-
-          <button>Ok</button>
-        </div>
-
-        <div className="App-footer">
-          <p>Copyright {currentYear} - {footerText}</p>
-        </div>
+    <div className="App">
+      <div className="root-notifications">
+        <Notifications />
       </div>
-    </>
+
+      <div className="App-header">
+        <img src={HbSLogo} alt="holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+
+        <button>Ok</button>
+      </div>
+
+      <div className="App-footer">
+        <p>Copyright {currentYear} - {footerText}</p>
+      </div>
+    </div>
   );
 }
 
