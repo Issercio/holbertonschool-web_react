cda83797e02428b7d42c83ac3d9d0a20d5efdb0e
Issercio <didi_1374@outlook.fr>
2026-05-15 10:20:11 +0200

    add the project

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -2,13 +2,12 @@ import HbSLogo from './assets/holberton-logo.jpg';
 import { getCurrentYear, getFooterCopy } from './utils';
 import Notifications from './Notifications.jsx';
 
-export default function App() {
+function App() {
   const currentYear = getCurrentYear();
   const footerText = getFooterCopy(true);
 
   return (
     <div className="App">
-
       <div className="App-header">
         <img src={HbSLogo} alt="holberton logo" />
         <h1>School dashboard</h1>
@@ -20,10 +19,10 @@ export default function App() {
         <p>Login to access the full dashboard</p>
 
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" />
+        <input id="email" type="email" name="email" />
 
         <label htmlFor="password">Password</label>
-        <input id="password" type="password" />
+        <input id="password" type="password" name="password" />
 
         <button>Ok</button>
       </div>
@@ -31,7 +30,8 @@ export default function App() {
       <div className="App-footer">
         <p>Copyright {currentYear} - {footerText}</p>
       </div>
-
     </div>
   );
 }
+
+export default App;
